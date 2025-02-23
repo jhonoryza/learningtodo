9dd70c7a80dcac3db47a8aedc349ab8235348b19
fajar sidik priatna <jardik.oryza@gmail.com>
2025-02-24 00:56:36 +0700

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +23,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+
+        if ($this->app->environment('production') && ! Str::contains(request()->url(), 'local')) {
+            URL::forceScheme('https');
+        }
     }
 }
