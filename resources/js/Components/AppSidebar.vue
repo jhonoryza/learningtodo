21c97250de0c697e3c7b5b441de4cb8f6589c581
fajar sidik priatna <jardik.oryza@gmail.com>
2025-02-23 15:14:29 +0700

    refactor using shadcn-vue with blue theme

---
+++ resources/js/Components/AppSidebar.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from '@/components/ui/sidebar';
+import { Link } from '@inertiajs/vue3';
+import { Home } from 'lucide-vue-next';
+
+// Menu items.
+const items = [
+    {
+        title: 'Dashboard',
+        url: route('dashboard'),
+        icon: Home,
+    },
+];
+</script>
+
+<template>
+    <Sidebar>
+        <SidebarHeader>
+            <Link :href="route('dashboard')">
+            <ApplicationLogo class="block h-9 w-auto" />
+            </Link>
+        </SidebarHeader>
+
+        <SidebarContent>
+            <SidebarGroup />
+            <SidebarGroupLabel>Learning Todo App</SidebarGroupLabel>
+            <SidebarGroupContent>
+                <SidebarMenu>
+                    <SidebarMenuItem v-for="item in items" :key="item.title">
+                        <SidebarMenuButton asChild>
+                            <a :href="item.url">
+                                <component :is="item.icon" />
+                                <span>{{ item.title }}</span>
+                            </a>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarGroupContent>
+            <SidebarGroup />
+        </SidebarContent>
+
+        <SidebarFooter>
+            <SidebarMenu>
+                <SidebarMenuItem>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <SidebarMenuButton>
+                                <User2 /> {{ $page.props.auth.user.name }}
+                                <ChevronUp class="ml-auto" />
+                            </SidebarMenuButton>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent side="top" class="w-[--reka-popper-anchor-width]">
+                            <DropdownMenuItem>
+                                <a :href="route('profile.edit')">
+                                    <span>Account</span>
+                                </a>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <a :href="route('logout')">
+                                    <span>Sign out</span>
+                                </a>
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </SidebarMenuItem>
+            </SidebarMenu>
+        </SidebarFooter>
+    </Sidebar>
+</template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,29 +1,34 @@
 <script setup lang="ts">
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb'; /* PartiallyEnd: #3632/scriptSetup.vue */
+import { Button } from '@/components/ui/button'; /* PartiallyEnd: #3632/scriptSetup.vue */
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
+
     <Head title="Dashboard" />
 
     <AuthenticatedLayout>
-        <template #header>
-            <h2
-                class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200"
-            >
-                Dashboard
-            </h2>
-        </template>
-
         <div class="py-12">
-            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div
-                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800"
-                >
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        You're logged in!
-                    </div>
-                </div>
+            <div class="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
+                <Breadcrumb>
+                    <BreadcrumbList>
+                        <BreadcrumbItem>
+                            <BreadcrumbLink :href="route('dashboard')">
+                                Dashboard
+                            </BreadcrumbLink>
+                        </BreadcrumbItem>
+                    </BreadcrumbList>
+                </Breadcrumb>
+                <Button>Test</Button>
             </div>
         </div>
     </AuthenticatedLayout>
